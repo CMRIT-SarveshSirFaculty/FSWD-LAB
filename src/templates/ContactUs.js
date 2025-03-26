9f1444629c0048a726426a91af64a17edad8cace
CMRIT-SarveshSirFaculty <sarvesh.ambala@live.com>
2025-03-26 07:01:44 +0530

    Delete src directory

--- src/templates/ContactUs.js
+++
@@ -1,5 +0,0 @@
-const ContactUs = () => {
-    return <h1>CMRIT, Kandlakoya village, Medchal, Hyderabad</h1>;
-  };
-  
-  export default ContactUs;
--- src/templates/Profile.js
+++
@@ -1,5 +0,0 @@
-const Profile = () => {
-    return <><h1>Welcome to Mrs. T.Jyotsna</h1><p>Assistant Professor, CSE Department, CMRIT</p></>
-  };
-  
-  export default Profile;
